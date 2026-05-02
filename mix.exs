57db44d4e8df261521699562ade9c88f2a1b308f
Pratik Khadloya <tispratik@gmail.com>
2026-05-02 16:46:06 +0530

    Add code reloader

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule HexMirror.MixProject do
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      listeners: [Phoenix.CodeReloader],
       deps: deps()
     ]
   end
